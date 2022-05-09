19ba80442f1c0f4dd3116c746410e982d9dde1d2
chen <569922982@qq.com>
2022-05-09 19:51:35 +0800

    up

--- resources/js/components/TagsView/index.vue
+++ resources/js/components/TagsView/index.vue
@@ -134,7 +134,7 @@ export default {
     },
     addTags () {
       const { name } = this.$route
-      if (name && path != '/' && getView(path)) {
+      if (name && path != '/' && this.getView(path)) {
         this.$store.dispatch('tagsView/addView', this.$route)
       }
       return false
