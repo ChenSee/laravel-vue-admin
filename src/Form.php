d82ebb87cdab030b7ec539b6eb72d8b8ef3fa1fe
chen <569922982@qq.com>
2022-05-27 20:13:51 +0800

    up

--- src/Form.php
+++ src/Form.php
@@ -880,7 +880,7 @@ class Form extends Component
             'ignoreEmptyProps' => $this->ignoreEmptyProps,
             'formItemLayout' => $this->formItemLayout,
             'tabPosition' => $this->tabPosition,
-            'tabDefaultName' => $this->tabDefaultName,
+            'tabDefaultName' => $this->tabDefaultName ?? $this->formItemLayout[0]['name'],
             'defaultValues' => (object)$this->formItemsValue,
             'formRules' => (object)$this->formRules,
             'ref' => $this->ref,
